fbe0de0f93f9b484ff5b3c8ef0713518b9118dc7
pancelor <pancelor@gmail.com>
2021-11-18 01:39:13 -0800

    wip color

--- min.lua
+++ min.lua
@@ -1,23 +1,27 @@
 pq"--"
 -- for i=0,15do if i^2%16==11 then pq(i,i^2) end end
-function xpq(a1,b1,c1)
- pq(a1,b1,c1)
+function xpq(a1,b1,c1,p)
+ pq(a1,b1,c1,"--",p)
  a1\=1
  b1\=1
  c1\=1
  ?"\f"..hex[a1]..a1.."\t\f"..hex[b1]..b1.."\t\f"..hex[c1]..c1
- if (a1==8 or a1==9) and b1==11 and c1==10 then pq("^^^") end
+ if (a1==8 or a1==9) and (b1==11 or b1==3) and c1==10 then pq("^^^\n^^^\n^^^\n^^^\n^^^\n^^^\n^^^\n^^^\n^^^\n^^^\n^^^\n^^^\n") end
 end
 function f(x,p)
- return ((x)^p)%16
+ return (x%p-6)%16
 end
 a="-5"b="+1"c="+10"
-for p in all{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,ˇ}do xpq(f(a,p),f(b,p),f(c,p)) end 
+for p in all{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,ˇ}do xpq(f(a,p),f(b,p),f(c,p),p) end 
 pq("goal: 8 11 10")
-stop()
+-- stop()
 
+-- ord(d)^♥
 -- d*-5
 -- d*11
+-- (d+9)^^9
+-- atan2(6,d)*12
+-- d%10-6
 
 poke(13-😐,3)sc=0sz=0fl={}rf=circfill
 ::g::bln,fc=0px=3py=3
@@ -35,7 +39,7 @@ for i=2,bln,2do
 d=q and"-5"v=@(i+1)*9u=@i*9rf(u+4,v+4,3,i/2|8)
 if(py==v\9and @i==px)px,py,fc=ox,oy poke2(i,%bln)bln-=2d="+1"?"⁷i6v1d1"
 if(bln<1)q=1d="+10"?"⁷ceg4⁶4"
-if(d)add(fl,{d,u,v,ord(d)^♥})sz+=d
+if(d)add(fl,{d,u,v,d*11})sz+=d
 end
 for f in all(fl)do
 f[3]-=1?unpack(f)
