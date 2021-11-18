09ff5911b5236cf6e3e3d853518c39fdfe35667e
pancelor <pancelor@gmail.com>
2021-11-17 21:28:33 -0800

    function float_add; save ~11 chars; 914

--- min.lua
+++ min.lua
@@ -1,26 +1,20 @@
--- poke(23-😐,16) @-1
 poke(13-😐,3)
-scr,scrz,br=0,0,0
-float={}
-
+sc,sz,br=0,0,0
+fl={}
 ::g::
 bln=0
 px=3
 py=3
+function fa(x,y,n)add(fl,{x=x,y=y,z=y-7,n=n})end
 for i=0,48 do
- x,y=i%7,i\7
- if rnd()<.25 and i~=24 then
-  -- todo: peek/poke instead? use map instead? (just need to know count...)
-  poke(bln,x,y)
-  bln+=2
- end
+ x,y=i%7,i\7if(rnd()<.25and i~=24)poke(bln,x,y)bln+=2
 end
 ::_::
 ?"⁶1⁶c5"
 rectfill(0,t(),64,64,1)
-d=sgn(scrz-scr)
-if(scrz~=scr)scr+=d d+=5
-print("⁶w⁶t"..scr,24,27,5+d)
+d=sgn(sz-sc)
+if(sz~=sc)sc+=d d+=5
+print("⁶w⁶t"..sc,24,27,5+d)
 
 b=btnp()
 if(b\16>0)b=-1
@@ -43,19 +37,19 @@ for i=bln-2,0,-2 do
  if x==px and y==py then
   px,py,fc=ox,oy
   bln-=2poke2(i,%bln)
-  add(float,{x=x*8,y=y*8,z=y*8-7,n="+1"})
+  fa(x*8,y*8,"+1")
   ?"⁷i6v1d1"
  end
- if(b<0)add(float,{x=x*8,y=y*8,z=y*8-7,n="-6"})
+ if(b<0)fa(x*8,y*8,"-6")
 end
-for f in all(float)do
+for f in all(fl)do
  print(f.n,f.x,f.y,ord(f.n)^♥)
  f.y+=f.z-f.y>>3
- if(f.y-f.z<1)del(float,f)scrz+=f.n
+ if(f.y-f.z<1)del(fl,f)sz+=f.n
 end
 
 if bln==0 then
- add(float,{x=24,y=24,z=17,n="+10"})
+ fa(24,24,"+10")
  ?"⁷ceg4⁶4"
  b=-1
 end
