2ceb4fa5252bf855139f36e6660db4493c91c97d
pancelor <pancelor@gmail.com>
2021-11-17 23:57:41 -0800

    extreme min; 715

--- min.lua
+++ min.lua
@@ -1,28 +1,22 @@
-poke(13-😐,3)sc=0sz=0fl={}rf=rectfill
-::g::bln=0px=3py=3
-function fa(x,y,n)sz+=n add(fl,{x,y,y-7,n})end
+poke(13-😐,3)B=0A=0C={}D=rectfill::g::E=0F=3G=3function H(x,y,n)B+=n add(C,{x,y,y-7,n})end
 for i=0,48do
-x,y=i%7,i\7if(rnd()<.25and i~=24)bln+=2poke(bln,x,y)
-end
-::_::d=sgn(sz-sc)ox=px?"⁶1⁶c5"
-if(sz~=sc)sc+=d d+=5
-b=btnp()rf(0,t(),64,64,1)oy=py?"⁶w⁶t"..sc,24,27,5+d
+x,y=i%7,i\7if(rnd()<.25and i~=24)E+=2poke(E,x,y)end::_::d=sgn(B-A)I=F?"⁶1⁶c5"
+if(B~=A)A+=d d+=5
+b=btnp()D(0,t(),64,64,1)J=G?"⁶w⁶t"..A,24,27,5+d
 if(b\16>0)b=-1
-if(b>0)fcn=b*.5938&.75
-if(fc)px+=cos(fc)py+=sin(fc)else fc,fcn=fcn
-if(abs(px-3)>3 or abs(py-3)>3)px,py,fc=ox,oy
-?"★⁵8d🐱",px*9+1,py*9+4,7
-for i=2,bln,2do
-y=@(i+1)u=@i*9v=y*9
-if(rnd()<.01)fillp(rnd()<<16)
-rf(1+u,1+v,u+8,v+8,i/2|8)fillp()
-if(@i==px and y==py)px,py,fc=ox,oy poke2(i,%bln)bln-=2fa(u,v,"+1")?"⁷i6v1d1"
-if(b<0)fa(u,v,"-6")
-end
-for f in all(fl)do
- x,y,z,n=unpack(f)d=y-z?n,x,y,ord(n)^♥
- f[2]-=d>>3del(fl,d<1and f)
-end
-if(bln==0)b=-1fa(24,24,"+10")?"⁷ceg4⁶4"
+if(b>0)K=b*.5938&.75
+if(L)F+=cos(L)G+=sin(L)else L,K=K
+if(abs(F-3)>3 or abs(G-3)>3)F,G,L=I,J
+?"★⁵8d🐱",F*9+1,G*9+4,7
+for i=2,E,2do
+y=@(i+1)u=@i*9v=y*9M=fillp
+if(rnd()<.01)M(rnd()<<16)
+D(1+u,1+v,u+8,v+8,i/2|8)M()
+if(@i==F and y==G)F,G,L=I,J poke2(i,%E)E-=2H(u,v,"+1")?"⁷i6v1d1"
+if(b<0)H(u,v,"-6")end
+for f in all(C)do
+x,y,z,n=unpack(f)d=y-z?n,x,y,ord(n)^♥
+f[2]-=d>>3del(C,d<1and f)end
+if(E==0)b=-1H(24,24,"+10")?"⁷ceg4⁶4"
 if(b==-1)goto g
 if(t()<64)goto _
