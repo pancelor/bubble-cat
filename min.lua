b6a44636b85a0cdd1da187133ab030384e8ded2a
pancelor <pancelor@gmail.com>
2021-11-18 00:51:59 -0800

    better quit check; 660

--- min.lua
+++ min.lua
@@ -1,12 +1,11 @@
 poke(13-😐,3)sc=0sz=0fl={}rf=circfill
-::g::bln=0px=3py=3
+::g::bln,fc=0px=3py=3
 for i=0,48do
 x,y=i%7,i\7if(rnd()<.25and i~=24)bln+=2poke(bln,x,y)
 end
-::_::b=btnp()d=sgn(sz-sc)ox=px?"⁶1⁶c"
+::_::b=btnp()q=b>16d=sgn(sz-sc)ox=px?"⁶1⁶c"
 if(sz~=sc)sc+=d d+=6
 rf(32,32,64-t(),1)oy=py?"⁶w⁶t"..sc,24,27,4+d
-if(b\16>0)b=-1
 if(b>0)fcn=b*.5938&.75
 if(fc)px+=cos(fc)py+=sin(fc)else fc,fcn=fcn
 if(abs(px-3)>3or abs(py-3)>3)px,py,fc=ox,oy
@@ -14,12 +13,12 @@ if(abs(px-3)>3or abs(py-3)>3)px,py,fc=ox,oy
 for i=2,bln,2do
 v=@(i+1)*9u,d=@i*9rf(u+4,v+4,3,i/2|8)
 if(py==v\9and @i==px)px,py,fc=ox,oy poke2(i,%bln)bln-=2d="+1"?"⁷i6v1d1"
-if(b<0)d="-6"
-if(bln==0)b=-1d="+10"?"⁷ceg4⁶4"
+if(q)d="-6"
+if(bln==0)q=1d="+10"?"⁷ceg4⁶4"
 if(d)add(fl,{u,v,d})sz+=d
 end
 for f in all(fl)do
 x,y,n=unpack(f)f[2]-=1?n,x,y,ord(n)^♥
 end
-if(b<0)goto g
+if(q)goto g
 if(t()<64)goto _
