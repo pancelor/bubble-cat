94d94e00d3326ece03b61d97f61686b253335a34
pancelor <pancelor@gmail.com>
2021-11-18 00:45:55 -0800

    Revert "extreme min"; 671
    This reverts commit f5e3ba7fb9bd04a094b676344a54e48d08ef0044.

--- min.lua
+++ min.lua
@@ -1,21 +1,24 @@
-poke(13-😐,3)B=0A=0C={}D=circfill::g::E=0F=3G=3for i=0,48do
-x,y=i%7,i\7if(rnd()<.25and i~=24)E+=2poke(E,x,y)
-end::_::b=btnp()d=sgn(A-B)J=F?"⁶1⁶c"
-if(A~=B)B+=d d+=6
-D(32,32,64-t(),1)K=G?"⁶w⁶t"..B,24,27,4+d
-if(b\16>0)b=-1
-if(b>0)H=b*.5938&.75
-if(I)F+=cos(I)G+=sin(I)else I,H=H
-if(abs(F-3)>3or abs(G-3)>3)F,G,I=J,K
-?"🐱",F*9+1,G*9+2,7
-for i=2,E,2do
-v=@(i+1)*9u,d=@i*9D(u+4,v+4,3,i/2|8)
-if(G==v\9and @i==F)F,G,I=J,K poke2(i,%E)E-=2d="+1"?"⁷i6v1d1"
-if(b<0)d="-6"
-if(E==0)b=-1d="+10"?"⁷ceg4⁶4"
-if(d)add(C,{u,v,d})A+=d
+poke(13-😐,3)sc=0sz=0fl={}rf=circfill
+::g::bln=0px=3py=3
+for i=0,48do
+x,y=i%7,i\7if(rnd()<.25and i~=24)bln+=2poke(bln,x,y)
 end
-for f in all(C)do
+::_::b=btnp()d=sgn(sz-sc)ox=px?"⁶1⁶c"
+if(sz~=sc)sc+=d d+=6
+rf(32,32,64-t(),1)oy=py?"⁶w⁶t"..sc,24,27,4+d
+if(b\16>0)b=-1
+if(b>0)fcn=b*.5938&.75
+if(fc)px+=cos(fc)py+=sin(fc)else fc,fcn=fcn
+if(abs(px-3)>3or abs(py-3)>3)px,py,fc=ox,oy
+?"🐱",px*9+1,py*9+2,7
+for i=2,bln,2do
+v=@(i+1)*9u,d=@i*9rf(u+4,v+4,3,i/2|8)
+if(py==v\9and @i==px)px,py,fc=ox,oy poke2(i,%bln)bln-=2d="+1"?"⁷i6v1d1"
+if(b<0)d="-6"
+if(bln==0)b=-1d="+10"?"⁷ceg4⁶4"
+if(d)add(fl,{u,v,d})sz+=d
+end
+for f in all(fl)do
 x,y,n=unpack(f)f[2]-=1?n,x,y,ord(n)^♥
 end
 if(b<0)goto g
