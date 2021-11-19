0ac45c833c3ef96207916e949a8b16e76c04bce7
pancelor <pancelor@gmail.com>
2021-11-18 16:14:32 -0800

    temp min; 598

--- min.lua
+++ min.lua
@@ -1,5 +1,5 @@
-poke(13-😐,3)fl={}sc=0sz=0rf=circfill::g::bn,fc=1px=3py=3for j=0,12do
-poke(bn,rnd(49))bn+=1end
+p=poke p(13-😐,3)fl={}sc=0sz=0rf=circfill::g::A,fc=1px=3py=3for j=0,12do
+p(A,rnd(49))A+=1end
 while t()<64do
 b=btnp()q=b>16and"⁷fb2"d=sgn(sz-sc)ox=px?"⁶1⁶c"
 if(sz~=sc)sc+=d d+=6
@@ -8,10 +8,10 @@ if(b>0)fn=b*.5938&.75
 if(not fc)fc,fn=fn
 if(fc)px+=cos(fc)py+=sin(fc)
 ty=abs(px-3)>3or abs(py-3)>3?"★⁵8d🐱",px*9+1,py*9+3,7
-for i=bn,1,-1do
+for i=A,1,-1do
 d=q and"-5"v=@i\7*9u=@i%7*9rf(u+4,v+4,3,i/2|8)
-if(py==v\9and u\9==px)ty=1poke(i,@bn)bn-=1d="+1"?"⁷d"
-if(bn<1)d="+10"q="⁷cg"
+if(py==v\9and u\9==px)ty=1p(i,@A)A-=1d="+1"?"⁷d"
+if(A<1)d="+10"q="⁷cg"
 if(d)add(fl,{d,u,v,d*11})sz+=d
 end
 if(ty)px,py,fc=ox,oy
