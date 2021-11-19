68d222de9ac811fa947a54752bfbdf4a288d16ca
pancelor <pancelor@gmail.com>
2021-11-18 16:38:42 -0800

    save another 2 from getting rid of poke; 595

--- min.lua
+++ min.lua
@@ -1,15 +1,15 @@
 poke(13-😐,3)fl={}sc=0sz=0rf=circfill::g::A,fc={}px=3py=3for j=0,12do
 add(A,rnd(49)\1)end
-while t()<64do
+while t()<60do
 b=btnp()q=b>16and"⁷fb2"d=sgn(sz-sc)ox=px?"⁶1⁶c"
 if(sz~=sc)sc+=d d+=6
-rf(32,32,64-t(),1)oy=py?"⁶w⁶t"..sc,24,27,4+d
+rf(32,32,60-t(),1)oy=py?"⁶w⁶t"..sc,24,27,4+d
 if(b>0)fn=b*.5938&.75
 if(not fc)fc,fn=fn
 if(fc)px+=cos(fc)py+=sin(fc)
 ty=abs(px-3)>3or abs(py-3)>3?"★⁵8d🐱",px*9+1,py*9+3,7
 for i=#A,1,-1do
-w=A[i]d=q and"-5"v=w\7*9u=w%7*9rf(u+4,v+4,3,i/2|8)
+w=A[i]d=q and"-5"v=w\7*9u=w%7*9rf(u+4,v+4,3,i|8)
 if(py==v\9and u\9==px)ty=del(A,w)d="+1"?"⁷d"
 if(#A<1)d="+10"q="⁷cg"
 if(d)add(fl,{d,u,v,d*11})sz+=d
