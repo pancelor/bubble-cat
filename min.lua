1832b614a8780383544d1e20d6529482b9bfd291
pancelor <pancelor@gmail.com>
2021-11-18 22:01:05 -0800

    rename

--- min.lua
+++ min.lua
@@ -1,7 +1,7 @@
 fl={}sc=0sz=0rf=circfill::_::bn,fc={}px=3py=3for j=0,12do?"⁶!5f2c³"
 add(bn,rnd(49)\1)end
 while t()<60do
-b=btnp()qq=b>8and"⁷fb2"d=sgn(sz-sc)ox=px?"⁶1⁶c"
+b=btnp()qu=b>8and"⁷fb2"d=sgn(sz-sc)ox=px?"⁶1⁶c"
 if(sz~=sc)sc+=d d+=6
 rf(32,32,60-t(),1)oy=py?"⁶w⁶t"..sc,24,27,4+d
 if(b>0)fn=b*.5938&.75
@@ -9,15 +9,15 @@ if(not fc)fc,fn=fn
 if(fc)px+=cos(fc)py+=sin(fc)
 ty=abs(px-3)>3or abs(py-3)>3?"★⁵8d🐱",px*9+1,py*9+3,7
 for j=#bn,1,-1do
-i=bn[j]d=qq and"-5"b=i\7*9a=i%7*9rf(a+4,b+4,3,j|8)
+i=bn[j]d=qu and"-5"b=i\7*9a=i%7*9rf(a+4,b+4,3,j|8)
 if(py*7+px==i)ty=del(bn,i)d="+1"?"⁷d"
-if(#bn<1)d="+10"qq="⁷cg"
+if(#bn<1)d="+10"qu="⁷cg"
 if(d)add(fl,{d,a,b,d*11})sz+=d
 end
 if(ty)px,py,fc=ox,oy
 for a in all(fl)do
 a[3]-=1?unpack(a)
 end
-if(qq)?qq
-if(qq)goto _
+if(qu)?qu
+if(qu)goto _
 end
