78715c34b20aee446855e14621a22f464c80e562
pancelor <pancelor@gmail.com>
2021-11-19 14:23:23 -0800

    better button handling and reset; 612

--- min.lua
+++ min.lua
@@ -1,10 +1,10 @@
-fl={}sy=0sz=0rf=circfill::_::bn,fc={}px=3py=3for j=0,12do?"⁶!5f2cC"
+px=3py=3fl={}sy=0sz=0rf=circfill::_::bn={}for j=0,12do?"⁶!5f2cC"
 add(bn,rnd(49)\1)end
 while t()<60do?"⁶1⁶c"
 b=btnp()qu=b>8and"⁷fb2"d=sgn(sz-sy)ox=px
 if(sz~=sy)sy+=d d+=6
 rf(32,32,60-t(),1)oy=py?"⁶w⁶t"..sy,24,27,4+d
-if(b>0)fn=b*.5938&.75
+if(b%16>0)fn=b*.5938&.75
 if(not fc)fc,fn=fn
 if(fc)px+=cos(fc)py+=sin(fc)
 st=px\7+py\7~=0?"★⁵8d🐱",px*9+1,py*9+3,7
