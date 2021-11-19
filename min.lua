3292391ded8b3807fd1897519a3b6213d1283987
pancelor <pancelor@gmail.com>
2021-11-19 14:56:17 -0800

    small cleanup

--- min.lua
+++ min.lua
@@ -1,5 +1,5 @@
-px=3py=3fl={}sy=0sz=0rf=circfill::_::bn={}for j=1,13do?"⁶!5f2cC"
-bn[j]=rnd(49)\1end
+px=3py=3fl={}sy=0sz=0rf=circfill::_::bn={}for j=1,13do
+bn[j]=rnd(49)\1end?"⁶!5f2cC"
 while t()<60do?"⁶1⁶c"
 b=btnp()qu=b>8and"⁷fb2"d=sgn(sz-sy)ox=px
 if(sz~=sy)sy+=d d+=6
@@ -7,13 +7,13 @@ rf(32,32,60-t(),1)oy=py?"⁶w⁶t"..sy,24,27,4+d
 if(b%16>0)fn=b*.5938&.75
 if(not fc)fc,fn=fn
 if(fc)px+=cos(fc)py+=sin(fc)
-st=px\7+py\7~=0?"★⁵8d🐱",px*9+1,py*9+3,7
+st=px\7+py\7~=0
 for j=#bn,1,-1do
 i=bn[j]d=qu and"-5"b=i\7*9a=i%7*9rf(a+4,b+4,3,j|8)
 if(py*7+px-px\7==i)st=del(bn,i)d="+1"?"⁷d"
 if(#bn<1)d="+10"qu="⁷cg"
 if(d)add(fl,{d,a,b,d*11})sz+=d
-end
+end?"★⁵8d🐱",px*9+1,py*9+3,7
 if(st)px,py,fc=ox,oy
 for a in all(fl)do
 a[3]-=1?unpack(a)
